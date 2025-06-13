335a38242d5838e6c7e69ca6b7eaca5db92a762c
jforres1 <forrej@gmail.com>
2025-06-13 15:22:54 -0400

    Added Normal Map Testing and cart to iso conversion

--- scenes/library/player_character.gd
+++ scenes/library/player_character.gd
@@ -2,10 +2,14 @@ extends CharacterBody2D
 
 const SPEED = 60.0
 
+func cartesian_to_isometric(cart: Vector2) -> Vector2:
+	return Vector2(cart.x - cart.y, (cart.x + cart.y) / 2)
+
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction:
 		direction.y /= 2
+		#direction = cartesian_to_isometric(direction)
 		direction = direction.normalized()
 		self.velocity = direction * SPEED
 	else:
