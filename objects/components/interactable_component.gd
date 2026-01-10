e19bde0c0b06eec7bdfc850137902ebe3a843656
jforres1 <forrej@gmail.com>
2026-01-09 22:19:44 -0500

    Preparation for chest inventory work

--- objects/components/interactable_component.gd
+++ objects/components/interactable_component.gd
@@ -1,8 +1,21 @@
 class_name Interactable
 extends Area2D
 
+@onready var interactable_area: CollisionPolygon2D = $InteractableArea
+var hovered: bool = false
+
 signal interacted(root: Node, interactor: Interactor)
 
-func interact(interactor: Interactor):
-    interacted.emit(self, interactor)
-    print("interacted with {0}".format([self.name]))
+func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
+func _on_mouse_exited() -> void:
+	hovered = false
+
+func _on_mouse_entered() -> void:
+	hovered = true
+	
+func interact(interactor: Interactor) -> void:
+	interacted.emit(self, interactor)
+	print("interacted with {0}".format([self.name]))
--- objects/components/interactor_component.gd
+++ objects/components/interactor_component.gd
@@ -2,26 +2,46 @@ class_name Interactor
 extends Area2D
 
 var interactables_list: Array[Interactable] = []
+@onready var label_container: BoxContainer = $BoxContainer
 
 func _ready() -> void:
-    area_entered.connect(_on_area_entered)
-    area_exited.connect(_on_area_exit)
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exit)
+
+func _sort_by_closest_hovered(a: Interactable, b: Interactable) -> bool:
+	if a.hovered and not b.hovered:
+		return true
+	elif b.hovered and not a.hovered:
+		return false
+	elif a.global_transform.origin.distance_to(self.global_transform.origin) \
+	< b.global_transform.origin.distance_to(self.global_transform.origin):
+		return true
+	else:
+		return false
+
+func _physics_process(delta: float) -> void:
+	interactables_list.sort_custom(_sort_by_closest_hovered)
+	if interactables_list.size() > 0:
+		label_container.visible = true
+		label_container.global_position = interactables_list[0].global_position
+	else:
+		label_container.visible = false
 
 func _input(event: InputEvent) -> void:
-    if not event is InputEventKey or not event.is_action_pressed("interact"): return
-    if interactables_list.size() > 0:
-        interactables_list[0].interact(self as Interactor)
+	if not event is InputEventKey or not event.is_action_pressed("interact"): return
+	if interactables_list.size() > 0:
+		interactables_list[0].interact(self as Interactor)
 
 func _on_area_entered(area: Area2D) -> void:
-    if area is Interactable:
-        print("entered {0} interaction area".format([area.name]))
-        interactables_list.append(area as Interactable)
-        print("{0} interactable objects".format([interactables_list.size()]))
+	if area is Interactable:
+		print("entered {0} interaction area".format([area.name]))
+		interactables_list.append(area as Interactable)
+		print("{0} interactable objects".format([interactables_list.size()]))
 
 func _on_area_exit(area: Area2D) -> void:
-    if area is Interactable:
-        print("exited {0} interaction area".format([area.name]))
-        var index = interactables_list.find(area)
-        if index > -1:
-            interactables_list.remove_at(index)
-            print("{0} remaining interactable objects".format([interactables_list.size()]))
+	if area is Interactable:
+		print("exited {0} interaction area".format([area.name]))
+		var index = interactables_list.find(area)
+		if index > -1:
+			interactables_list.remove_at(index)
+			print("{0} remaining interactable objects".format([interactables_list.size()]))
